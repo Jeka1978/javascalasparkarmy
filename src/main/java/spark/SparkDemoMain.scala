fd03fd69245cbf89ec5b6433b65961c4ffeaf311
jeka1978 <kit2009>
2019-12-24 15:35:32 +0200

    spark lab

--- src/main/java/spark/SparkDemoMain.scala
+++ src/main/java/spark/SparkDemoMain.scala
@@ -9,7 +9,7 @@ object SparkDemoMain {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("demo").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val rdd = sc.parallelize(List(1,2,3))
-
+    val rdd = sc.textFile("/data/taxi_orders.txt")
+    rdd.foreach(println(_))
   }
 }
